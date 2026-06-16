108ab3e4c8cf223bcf4096a6f29aa749549e8779
sheetal3440-arch <sheetal3440@gmail.com>
2026-06-16 21:34:50 +0530

    Upload day16

---
+++ day16/q63.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, sum;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter the required sum: ";
+    cin >> sum;
+
+    bool found = false;
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] + arr[j] == sum) {
+                cout << "Pair found: " << arr[i] << " and " << arr[j] << endl;
+                found = true;
+            }
+        }
+    }
+
+    if (!found) {
+        cout << "No pair found with the given sum.";
+    }
+
+    return 0;
+}
---
+++ day16/q64.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; ) {
+            if(arr[i] == arr[j]) {
+                for(int k = j; k < n - 1; k++) {
+                    arr[k] = arr[k + 1];
+                }
+                n--;
+            } else {
+                j++;
+            }
+        }
+    }
+
+    cout << "Array after removing duplicates: ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
